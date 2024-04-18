e655e4dd8964203aa339e5ccb181e1af5ddf558c
NihatHesenli <161558388+NihatHesenli@users.noreply.github.com>
2024-04-18 14:17:29 -0700

    Add files via upload

---
+++ HOTEL TASK/ConsoleApp5/HOTEL.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Hotel
+    {
+        public int id {  get; set; }
+        public string name { get; set; }
+        List<Room> rooms { get; set; }
+        public Hotel(string name)
+        {
+            this.name = name;
+            rooms = new List<Room>();
+
+        }
+        public void AddRoom(Room room)
+        {
+            rooms.Add(room);
+        }
+        public void MakeReservation(int roomId)
+        {
+
+            if (roomId = null) new NullReferenceException("Id sehv daxil olunub");
+
+            foreach (Room room in rooms)
+            {
+                if (room.Id == roomId)
+                {
+                    if (room.IsAviable)
+                    {
+                        Console.WriteLine("rezerv olundu");
+                        room.IsAviable = false;
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("artiq rezerv olunub")
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ HOTEL TASK/ConsoleApp5/Room.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Room
+    {
+        public int Id;
+        public static int _id;
+        public string name {  get; set; }
+        public double price { get; set; }
+        public int PersonCapacity { get; set; }
+        public string IsAviable { get; set; }
+
+
+    }
+}
